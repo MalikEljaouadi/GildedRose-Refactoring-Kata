610dcd697218846da82e0c57b9090fb905f86b2f
Malik Eljaouadi <malik.eljaouadi@elyadata.com>
2023-01-01 13:44:37 +0100

    merge nested if conditions

--- python/gilded_rose_refactored.py
+++ python/gilded_rose_refactored.py
@@ -7,32 +7,29 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
             if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
-                if item.sell_in < 1:
-                    if item.quality < 50:
-                        item.quality += 1
+                if item.sell_in < 1 and item.quality < 50:
+                    item.quality += 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality += 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality += 1
                 if item.sell_in < 1:
                     item.quality = item.quality - item.quality
             else:
                 if item.quality > 0:
                     item.quality -= 1
-                if item.sell_in < 1:
-                    if item.quality > 0:
-                        item.quality -= 1
+                if item.sell_in < 1 and item.quality > 0:
+                    item.quality -= 1
             item.sell_in -= 1
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -41,4 +38,4 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        return f"{self.name}, {self.sell_in}, {self.quality}"
