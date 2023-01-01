dbf68b703b745dc2128207a529803f30dc741fde
Malik Eljaouadi <malik.eljaouadi@elyadata.com>
2023-01-01 12:39:30 +0100

    change an if else statement by elif

--- python/gilded_rose_refactored.py
+++ python/gilded_rose_refactored.py
@@ -27,13 +27,12 @@ class GildedRose(object):
                 if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality = item.quality + 1
-                else:
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                         item.quality = item.quality - item.quality
-                    else:
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                else:
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
 
 
 class Item:
