ebcbe920f52e57d66ebbc933f567badcdaf8ae5d
Malik Eljaouadi <malik.eljaouadi@elyadata.com>
2023-01-01 13:37:10 +0100

    swith the 'Aged' and 'Backsatge' to confirm the switch -like statement

--- python/gilded_rose_refactored.py
+++ python/gilded_rose_refactored.py
@@ -24,13 +24,15 @@ class GildedRose(object):
             else:
                 if item.quality > 0:
                     item.quality -= 1
-            if item.sell_in < 1:
-                if item.name == "Aged Brie":
+            if item.name == "Aged Brie":
+                if item.sell_in < 1:
                     if item.quality < 50:
                         item.quality += 1
-                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 1:
                     item.quality = item.quality - item.quality
-                else:
+            else:
+                if item.sell_in < 1:
                     if item.quality > 0:
                         item.quality -= 1
             item.sell_in -= 1
