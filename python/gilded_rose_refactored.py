6797a552b6776feefbda3ad93c3b963d4a656c14
Malik Eljaouadi <malik.eljaouadi@elyadata.com>
2023-01-01 12:46:25 +0100

    change the test on Sulfuras to make it positive

--- python/gilded_rose_refactored.py
+++ python/gilded_rose_refactored.py
@@ -30,7 +30,9 @@ class GildedRose(object):
                 elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = item.quality - item.quality
                 else:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name == "Sulfuras, Hand of Ragnaros":
+                        pass
+                    else:
                         if item.quality > 0:
                             item.quality = item.quality - 1
 
